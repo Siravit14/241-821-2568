d6e9add180823a77dc21f5a9adc505aab9a3c04b
Siravit14 <sirvit.po@ku.th>
2025-12-30 11:29:26 +0700

    add js

---
+++ js/main.js
@@ -0,0 +1,80 @@
+//console.log("hello world")
+//console.log("This is javaScript file.")
+
+/*
+let name = "john";
+let age = 5.9;
+let height = 169;
+const PI = 3.14;
+name = "Buakaw";
+console.log("Name:",name);
+console.log("Age:",age);
+console.log("Height:",height);
+*/
+
+// +-*/
+/*
+let number1 = 10;
+let number2 = 3;
+let result = number1+number2;
+console.log("ผลบวก:",result); 
+*/
+/*
+let number1 = 5;
+let number2 = 3;
+let condition = number1 <= number2;
+console.log("condition:",condition);
+
+if (number1 >= number2) {
+    console.log("num1 >= num2");
+} else if (number1 == number2) {
+    console.log("this is else if");
+} else {
+    console.log("this is else");
+}
+*/
+/*
+let SCORE = prompt("enter your score:");
+if (SCORE >=80) {
+    console.log("you got grade A");
+} else if (SCORE >=70) {
+    console.log("you got grade B");
+} else if (SCORE >=60) {
+    console.log("you got grade C");
+} else if (SCORE >=50) {
+    console.log("you got grade D");
+} else {
+    console.log("you got grade F");
+}
+*/
+/*
+let number1=5;
+let number2=10;
+
+let condition = (number1 > 0) || (number2 >0);
+console.log("condition:",condition);
+
+let age = 25;
+let gander = "female";
+if (age == 18 && gender == "male") {
+    console.log("คุณไป รด ได้");
+}
+    */
+/*
+let num1 = 20;
+if (num1 %2 ==0 ) {
+    console.log("even");
+} else {
+    console.log("odd");
+}
+    */
+
+let count =0;
+while(count<=5) {
+    console.log("while",count)
+    count = count+1;
+}
+for (let i = 0;i<=4;i=i+1) {
+    console.log("for",i)
+
+}
